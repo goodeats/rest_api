f931214662f28d436bb69739b2dc15b7fa0f9b50
Pat Needham <patrick.h.needham@gmail.com>
2022-04-04 16:55:54 -0400

    CRUD: index and show methods

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ require('dotenv').config();
 // reference:
 // https://expressjs.com/en/5x/api.html
 
+// makeshift courses table
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World!!');
 });
 
 app.get('/api/courses', (req, res) => {
-  // no db yet, just demo res
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
@@ -19,7 +25,16 @@ app.get('/api/courses/:id', (req, res) => {
   // res.send(req.params);
 
   // /api/courses/1?sort=date #=> {sort: date}
-  res.send(req.query);
+  // res.send(req.query);
+
+  const course = courses.find(
+    (c) => c.id === parseInt(req.params.id)
+  );
+  if (!course)
+    res
+      .status(404)
+      .send('The course with the given ID was not found');
+  res.send(course);
 });
 
 // PORT
